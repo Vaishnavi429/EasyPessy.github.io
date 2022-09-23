922f4decb41c9df032b378604c30f833c06b432a
vaishnavi <guptavaish429@gmail.com>
2022-09-23 14:50:58 +0530

    added formik

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,47 +4,80 @@ import { Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import { Stack } from "@mui/system";
 import Button from "@mui/material/Button";
-import Link from "@mui/material/Link"
+import Link from "@mui/material/Link";
+import {Formik,Form} from "formik";
+import * as Yup from "yup";
 
 function Login() {
+  const loginValidation = Yup.object().shape({
+    email:Yup.string().email('Invalid email').required('enter email'),
+    password:Yup.string().required("enter password")
+  })
   return (
     <Fragment>
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
+      <Formik 
+      initialValues={{
+        email:"",
+        password:"",
       }}
-    >
-      <Box
-        sx={{
-          width: "25vw",
-          color: "black",
-          color:"#9C2C77",
-          background:"#CFD2CF",
-          border:"2px solid #5F6F94",
-          padding: 8,
+      validationSchema={loginValidation}
+      onSubmit={values=>{
+        alert(JSON.stringify(values, null, 2));
+      
+      }}>
+        {({values,handleChange,errors})=>
+        <Form >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
         }}
       >
-        <Stack spacing={2}>
-        <Typography variant="h4">Ba<span style={{color:"#16213E"}}>atCh</span>it</Typography>
-          <TextField id="filled" label="Email" variant="filled" />
-          <TextField
-            id="filled"
-            label="Password"
-            type="password"
-            variant="filled"
-          />
+        <Box
+          sx={{
+            width: "25vw",
+            height: "70vh",
+            color: "black",
+            color: "#9C2C77",
+            background: "#CFD2CF",
+            border: "2px solid #5F6F94",
+            padding: 8,
+          }}
+        >
+          <Stack spacing={2}>
+            <Typography variant="h4">
+              Ba<span style={{ color: "#16213E" }}>atCh</span>it
+            </Typography>
+            <TextField  label="Email" variant="filled" name="email"  
+             value={values.email}
+              onChange={handleChange}/>
+            
+            <TextField
+              label="Password"
+              type="password"
+              variant="filled"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+            />
           </Stack>
-        <Button variant="contained" color="success">
-          Login
-        </Button>
-        <Typography>Don't have account <Link href="/Register" >Register</Link></Typography>
-      </Box>
-    </div>
-  </Fragment>
-)
+          <div style= {{marginTop:"20px"}}>
+            <Button variant="contained" color="success" type="submit">
+              Login
+            </Button>
+            <Typography>
+              Don't have account <Link href="/Register">Register</Link>
+            </Typography>
+          </div>
+        </Box>
+      </div>
+      </Form>
+        }
+      </Formik>
+    </Fragment>
+  );
 }
 
-export default Login
+export default Login;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -5,11 +5,31 @@ import TextField from "@mui/material/TextField";
 import { Stack } from "@mui/system";
 import Button from "@mui/material/Button";
 import Link from "@mui/material/Link"
+import {Formik,Form} from "formik";
+import * as Yup from "yup";
 
 function Register() {
+  const RegisterValidation = Yup.object().shape({
+    user_name:Yup.string().required("enter user name"),
+    email: Yup.string().email("Invalid Email").required("enter email"),
+    password:Yup.string().required("enter password")
+  })
     
   return (
     <Fragment>
+       <Formik 
+      initialValues={{
+        user_name:"",
+        email:"",
+        password:"",
+      }}
+      validationSchema={RegisterValidation}
+      onSubmit={values=>{
+        alert(JSON.stringify(values, null, 2));
+      
+      }}>
+        {({values,handleChange,errors})=>
+        <Form >
       <div
         style={{
           display: "flex",
@@ -21,7 +41,7 @@ function Register() {
         <Box
           sx={{
             width: "25vw",
-            color: "black",
+            height:"70vh",
             color:"#9C2C77",
             background:"#CFD2CF",
             border:"2px solid #5F6F94",
@@ -31,27 +51,38 @@ function Register() {
           <Stack spacing={2}>
           <Typography variant="h4">Ba<span style={{color:"#16213E"}}>atCh</span>it</Typography>
             
-            <TextField id="filled" label="UserName" variant="filled" />
-            <TextField id="filled" label="Email" variant="filled" />
+            <TextField  label="UserName" variant="filled" 
+            name="user_name" 
+            value={values.user_name}
+            onChange={handleChange} />
+            <TextField  label="Email" variant="filled"
+              name="email" 
+              value={values.email}
+              onChange={handleChange} />
             <TextField
-              id="filled"
+             
               label="Password"
               type="password"
               variant="filled"
+              name="password" 
+              value={values.password}
+              onChange={handleChange}
             />
             <TextField
-              id="filled"
               label="Confirm Password"
               type="password"
               variant="filled"
+              
             />
           </Stack>
-          <Button variant="contained" color="success">
+          <div style = {{marginTop:"20px"}}>
+          <Button variant="contained" color="success" type="submit">
             Register
           </Button>
-          <Typography>already have account <Link href="/login" >Login</Link></Typography>
+          <Typography>already have account <Link href="/login" >Login</Link></Typography></div>
         </Box>
       </div>
+      </Form>}</Formik>
     </Fragment>
   );
 }
